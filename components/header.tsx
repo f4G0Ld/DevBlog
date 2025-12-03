d529dc6007e453e597668aff19af91fb5bf128f8
f4G0Ld <vasia.kharitonov@gmail.com>
2025-12-03 03:37:03 +0300

    package issue fix

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,19 @@
+import { Plus, SquarePen } from "lucide-react";
+
 export default function Header() {
 	return (
-		<div className="flex justify-between container p-4">
-			<div>
-				<p>DevBlog</p>
-			</div>
-			<div className="flex gap-5">
-				<a href="#">Home</a>
-				<a href="#">About</a>
-				<a href="#">Categories</a>
-				<button>
-					<p>Create Post</p>
-				</button>
+		<div className="bg-white w-full border-b border-[#C0C0C0]">
+			<div className="justify-between max-w-4xl mx-auto px-4 py-6">
+				<div className="flex items-center justify-between text-[14px]">
+					<div className="flex gap-3 items-center">
+						<SquarePen className="w-8 h-8 text-[#155DFC]" />
+						<p>DevBlog</p>
+					</div>
+						<button className="flex items-center px-3.5 py-1.75 gap-2 bg-[#155DFC] rounded-lg text-white">
+							<Plus className="w-4 h-4" />
+							<p>Create Post</p>
+						</button>
+				</div>
 			</div>
 		</div>
 	);
---
+++ lib/db/schema.ts
@@ -0,0 +1,61 @@
+import { relations } from 'drizzle-orm'
+import * as pg from 'drizzle-orm/pg-core'
+
+export const posts = pg.pgTable('posts', {
+    id: pg
+    .varchar({length:255})
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => Bun.randomUUIDv7()),
+
+    title: pg
+    .varchar({length:255})
+    .notNull(),
+    
+    name: pg
+    .varchar({length:255})
+    .notNull(),
+
+    description: pg
+    .text()
+    .notNull(),
+
+    likes: pg
+    .integer()
+    .default(0),
+
+    createdAt: pg
+    .timestamp()
+    .notNull()
+    .defaultNow()
+})
+
+export const comments = pg.pgTable('comments', {
+    id: pg
+    .varchar({length:255})
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => Bun.randomUUIDv7()),
+
+    name: pg
+    .text()
+    .notNull(),
+
+    text: pg
+    .text()
+    .notNull(),
+    
+    postId: pg
+    .varchar({length:255})
+    .notNull()
+    .references(() => posts.id, {onDelete:'cascade', onUpdate:'cascade'}),
+
+    likes: pg
+    .integer()
+    .default(0),
+
+    createdAt: pg
+    .timestamp()
+    .notNull()
+    .defaultNow(),
+})  
